c16dc09a703cbbbfd2e4cfa6087f25f9b78303ac
Fredrik Lax <Fredrik.Lax@yaelev.se>
2021-06-01 10:32:40 +0200

    initial commit

--- src/main/java/com/lax/carrental/dao/CarRepo.java
+++ src/main/java/com/lax/carrental/dao/CarRepo.java
@@ -1,11 +1,8 @@
 package com.lax.carrental.dao;
 
 import com.lax.carrental.entity.Cars;
-import com.sun.org.apache.xpath.internal.operations.Bool;
-import jdk.nashorn.internal.objects.annotations.Where;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -15,6 +12,7 @@ public interface CarRepo extends JpaRepository<Cars, Long> {
         Cars deleteById(long id);
         Cars findByName(String name);
         Cars findByType(String type);
+        Cars findByPrice(int price);
 
         @Query("select c from Cars c where c.type= :type AND c.booked= :isbooked")
         List<Cars> findWhere(String type, Boolean isbooked);
